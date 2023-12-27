00d17ed059781b3aaad92bb15e0895b3bf463bdb
Irirwanirira <irijoseph41@gmail.com>
2023-12-27 13:10:53 +0200

    Add necessary librarieas

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
 const express = require("express");
-
 const mongoose = require("mongoose");
-
 require('dotenv').config();
 const port  = process.env.PORT;
-const bodyParser = require("body-parser");
-const routes = require("./src/routes/routes");
-const creteServer = require("./server");
+const createServer = require("./server");
 
 
 mongoose.connect("mongodb://localhost:27017/apartment")
     .then(()=> {
-        const app = express();
-        app.use(bodyParser.json());
-        app.use("/api", routes);
+        const app = createServer();
         app.listen(port, ()=> {
             console.log(`Server is running on port ${port}`)
             console.log("MongoDb connected");
--- server.test.js
+++ server.test.js
@@ -1,8 +1,10 @@
 const  mongoose  = require("mongoose");
 const supertest = require("supertest");
-const setServer = require("../server");
-const Apartment = require("../src/model/apartmentModel");
-const {response} = require("express");
+const createServer = require("./server");
+const Apartment = require("./src/model/apartmentModel");
+const { response } = require("express");
+const { patch } = require("./src/routes/routes");
+const app = createServer();
 
 
 beforeEach( async()=> {
@@ -10,13 +12,12 @@ beforeEach( async()=> {
 })
 
 afterEach( async()=> {
-    await mongoose.connection.db.dropCollection(()=> {
-        mongoose.connection.close()
-    })
+    await Apartment.deleteMany();
+    await mongoose.connection.close();
 })
 
-test("GET /APARTMENT", async() => {
-    const post = await Apartment.create({
+test("GET /APARTMENTS", async() => {
+    const data = await Apartment.create({
         name: "grazer",
         location: "NY",
         rooms:50,
@@ -26,10 +27,129 @@ test("GET /APARTMENT", async() => {
         .get("/api/apartments")
         .expect(200)
         .then((response) => {
+            expect(Array.isArray(response.body)).toBeTruthy()
             expect(response.body.length).toBeTruthy()
-            expect(response.body[0]._id).toBe(post.id)
-            expect(response.body[0].name).toBe(post.name)
-            expect(response.body[0].location).toBe(post.location)
-            expect(response.body[0].rooms).toBe(post.rooms)
-            expect(response.body[0].full).toBe(post.full)})        
+            expect(response.body[0]._id).toBe(data.id)
+            expect(response.body[0].name).toBe(data.name)
+            expect(response.body[0].location).toBe(data.location)
+            expect(response.body[0].rooms).toBe(data.rooms)
+            expect(response.body[0].full).toBe(data.full)
+        })
+})
+
+test("POST /APARTMENTS", async() => {
+    const data = {
+        name: "grazer",
+        location: "NY",
+        rooms:50,
+        full: false
+    }
+    await supertest(app)
+        .post("/api/apartments")
+        .send(data)
+        .expect(201)
+        .then(async(response) => {
+            expect(response.body._id).toBeTruthy()
+            expect(response.body.name).toBe(data.name)
+            expect(response.body.location).toBe(data.location)
+            expect(response.body.rooms).toBe(data.rooms)
+            expect(response.body.full).toBe(data.full)
+            const post = await Apartment.findOne({_id: response.body._id})
+            expect(post).toBeTruthy()
+            expect(post.name).toBe(data.name)
+            expect(post.location).toBe(data.location)
+            expect(post.rooms).toBe(data.rooms)
+            expect(post.full).toBe(data.full)
+        })
+})
+
+test("GET /APARTMENTS/:ID", async() => {
+    const data = await Apartment.create({
+        name: "grazer",
+        location: "NY",
+        rooms:50,
+        full: false
+    })
+    await supertest(app)
+        .get(`/api/apartments/${data._id}`)
+        .expect(200)
+        .then((response) => {
+            expect(response.body._id).toBe(data.id)
+            expect(response.body.name).toBe(data.name)
+            expect(response.body.location).toBe(data.location)
+            expect(response.body.rooms).toBe(data.rooms)
+            expect(response.body.full).toBe(data.full)
+        })
+})
+
+test("PUT /APARTMENTS/:ID", async() => {
+    const data = await Apartment.create({
+        name: "grazer",
+        location: "NY",
+        rooms:50,
+        full: false
+    })
+    const update = {
+        full: true
+    }
+    await supertest(app)
+        .put(`/api/apartments/${data._id}`)
+        .send(update)
+        .expect(200)
+        .then(async(response) => {
+            expect(response.body._id).toBe(data.id)
+            expect(response.body.full).toBe(update.full)
+            const post = await Apartment.findOne({_id: response.body._id})
+            expect(post).toBeTruthy()
+            expect(post.full).toBe(update.full)
+        })
+})
+
+test("PATCH /APARTMENTS/:ID", async() => {
+    const data = await Apartment.create({
+        name: "grazer",
+        location: "NY",
+        rooms:50,
+        full: false
+    })
+    const update = {
+        name: "Genesis",
+        location: "G city",
+        rooms: 100,
+        full: true
+    }
+    await supertest(app)
+        .patch(`/api/apartments/${data._id}`)
+        .send(update)
+        .expect(200)
+        .then(async(response) => {
+            expect(response.body._id).toBe(data.id)
+            expect(response.body.name).toBe(update.name)
+            expect(response.body.location).toBe(update.location)
+            expect(response.body.rooms).toBe(update.rooms)
+            expect(response.body.full).toBe(update.full)
+            const post = await Apartment.findOne({_id: response.body._id})
+            expect(post).toBeTruthy()
+            expect(post.name).toBe(update.name)
+            expect(post.location).toBe(update.location)
+            expect(post.rooms).toBe(update.rooms)
+            expect(post.full).toBe(update.full)
+        })
+})
+
+test("DELETE /APARTMENTS/:ID", async() => {
+    const data = await Apartment.create({
+        name: "grazer",
+        location: "NY",
+        rooms:50,
+        full: false
+    })
+    await supertest(app)
+        .delete("/api/apartments/" + data.id)
+        .expect(200)
+        .then(async(response) => {
+            let id = response.body._id
+            const post = await Apartment.findOne({_id: id})
+            expect(post).toBeFalsy()
+        })
 })
